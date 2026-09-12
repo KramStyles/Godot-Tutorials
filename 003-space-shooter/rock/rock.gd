2f1aa8a0e9d13d46d95ed05b32d0e50ecc7a9bbc
kramstyles <kramstyles@outlook.com>
2026-09-12 10:26:45 +0100

    added rocks and bullet is shooting

---
+++ 003-space-shooter/rock/rock.gd
@@ -0,0 +1,28 @@
+extends RigidBody2D
+
+var screensize = Vector2.ZERO
+var size
+var radius
+var scale_factor = 0.2
+
+
+func start(_position, _velocity, _size):
+	position = _position
+	size = _size
+	mass = 1.5 * size
+	$Sprite2D.scale = Vector2.ONE * scale_factor * size
+	radius = int($Sprite2D.texture.get_size().x / 2 * $Sprite2D.scale.x)
+	var shape = CircleShape2D.new()
+	shape.radius = radius
+	$CollisionShape2D.shape = shape
+	linear_velocity = _velocity
+	angular_velocity = randf_range(-PI, PI)
+	
+
+func _integrate_forces(_state: PhysicsDirectBodyState2D) -> void:
+	var xform = _state.transform
+	# use wrapf to keep the position bounded between 0 & screensize(x,y).
+	# if it moves pass right, teleports to left and vice versa
+	xform.origin.x = wrapf(xform.origin.x, 0 - radius, screensize.x + radius)
+	xform.origin.y = wrapf(xform.origin.y, 0 - radius, screensize.y + radius)
+	_state.transform = xform
