dcd18f9e593d9651802671652a728535bd67148e
kramstyles <kramstyles@outlook.com>
2026-09-17 14:49:21 +0100

    added sound, music and score

--- 004-brackeys-godot-tutorial/scripts/coin.gd
+++ 004-brackeys-godot-tutorial/scripts/coin.gd
@@ -1,5 +1,11 @@
 extends Area2D
 
+# This would work because the game manager and coins are called in the same scene (main). 
+@onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 
 func _on_body_entered(body: Node2D) -> void:
-	queue_free()
+	game_manager.add_point()
+	#queue_free()  # Not using queue free. Let's call from animation player
+	animation_player.play("pickup")
