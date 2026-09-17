61732330d1310d058ea0d3d5b94ba2173267bd0a
kramstyles <kramstyles@outlook.com>
2026-09-17 10:17:02 +0100

    added player dying and enemy slime

--- 004-brackeys-godot-tutorial/scripts/killzone.gd
+++ 004-brackeys-godot-tutorial/scripts/killzone.gd
@@ -4,9 +4,13 @@ extends Area2D
 
 
 func _on_body_entered(body: Node2D) -> void:
+	Engine.time_scale = 0.5  # Slows stuff down
+	# Collider keeps it on the platform. Let the player fall off after dying.
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 
 
 func _on_timer_timeout() -> void:
 	# Reload the game
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
